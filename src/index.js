cd8593f209d0fc2b2b6ea9617788895485c800af
Veera Prakash <veera.p0820@gmail.com>
2021-04-24 15:21:01 -0500

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import 'dotenv/config'
+import cors from 'cors'
 import express from 'express'
 
 const app = express()
 const port = 8080
 
+
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
 app.listen(port, () => {
   console.log('Express App listening at Port:', port)
 })
