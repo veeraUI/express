95ac33c9eee0bf20ab571e47db760297f6aef80e
Veera Prakash <veera.p0820@gmail.com>
2021-04-24 18:55:40 -0500

    feat: add mongoose database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
-import models from '../models'
+import models, { connectDb } from './models';
 import routes from './routes'
 
 const app = express()
@@ -26,6 +26,54 @@ app.use('/users', routes.user)
 app.use('/messages', routes.message)
 app.use('/', routes.root)
 
-app.listen(port, () => {
-  console.log('Express App listening at Port:', port)
+const eraseDatabaseOnSync = true;
+
+ 
+const createUsersWithMessages = async () => {
+  const user1 = new models.User({
+    username: 'rwieruch',
+  });
+ 
+  const user2 = new models.User({
+    username: 'ddavids',
+  });
+ 
+  const message1 = new models.Message({
+    text: 'Published the Road to learn React',
+    user: user1.id,
+  });
+ 
+  const message2 = new models.Message({
+    text: 'Happy to release ...',
+    user: user2.id,
+  });
+ 
+  const message3 = new models.Message({
+    text: 'Published a complete ...',
+    user: user2.id,
+  });
+ 
+  await message1.save();
+  await message2.save();
+  await message3.save();
+ 
+  await user1.save();
+  await user2.save();
+};
+
+
+connectDb().then(async () => {
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Message.deleteMany({}),
+    ]);
+  }
+
+  createUsersWithMessages();
+
+  app.listen(port, () => {
+    console.log('Express App listening at Port:', port)
+  })
 })
+
